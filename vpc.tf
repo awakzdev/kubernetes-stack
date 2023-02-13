0a4170af1ef353b5c988d4ef7db0ae134e163e87
Elazar <96201125+awakzdev@users.noreply.github.com>
2023-02-13 12:02:59 +0200

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,26 +1,26 @@
-variable "project_id" {
+variable "gcp_project" {
   description = "project id"
 }
 
-variable "region" {
+variable "gcp_region" {
   description = "region"
 }
 
 provider "google" {
-  project = var.project_id
-  region  = var.region
+  project = var.gcp_project
+  region  = var.gcp_region
 }
 
 # VPC
 resource "google_compute_network" "vpc" {
-  name                    = "${var.project_id}-vpc"
+  name                    = "${var.gcp_project}-vpc"
   auto_create_subnetworks = "false"
 }
 
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.project_id}-subnet"
-  region        = var.region
+  name          = "${var.gcp_project}-subnet"
+  region        = var.gcp_region
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
 }
